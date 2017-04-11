61910255576ed5568a8164c75252a0453c6e072d
Bogdan Preda <bogdan.preda@themeisle.com>
2017-04-11 18:52:26 +0300

    WIP: Structure

---
+++ app/bot-curl-class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Bot_Curl {
+
+    private $url;
+
+    public function __construct( $url ) {
+        $this->url = $url;
+    }
+
+    public function get_http_code() {
+        $ch = curl_init( $this->url );
+        curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,10);
+        $output = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpcode;
+    }
+}
---
+++ bot.php
@@ -0,0 +1,10 @@
+<?php
+
+spl_autoload_register(function ( $class_name ) {
+    $class_file_name = strtolower( str_replace( '_', '-', $class_name ) ) . '-class';
+    include 'app/' . $class_file_name . '.php';
+});
+
+$bot_curl = new Bot_Curl( 'https://profiles.wordpress.org/themeisle/' );
+
+echo 'HTTP Status: ' . $bot_curl->get_http_code();
